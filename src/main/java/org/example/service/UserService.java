f5e7e4316b433cc19e42142e3dee1024d1d5786d
levin <ivan.levin@ocrv.ru>
2025-06-18 10:36:41 +0300

    CRUD

--- src/main/java/org/example/service/UserService.java
+++ src/main/java/org/example/service/UserService.java
@@ -2,7 +2,9 @@ package org.example.service;
 
 import java.util.List;
 import org.example.model.User;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 public interface UserService {
     List<User> listUsers();
 
