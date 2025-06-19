4d9c3b35041ad83d319464b4697157515eba882d
levin <ivan.levin@ocrv.ru>
2025-06-19 08:43:56 +0300

    CRUD

--- src/main/java/org/example/service/UserServiceImp.java
+++ src/main/java/org/example/service/UserServiceImp.java
@@ -5,6 +5,7 @@ import org.example.dao.UserDao;
 import org.example.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -17,23 +18,23 @@ public class UserServiceImp implements UserService{
     }
 
     @Override
-    @Transactional
+    @Transactional(readOnly = true)
     public List<User> listUsers() { return userDao.listUsers(); }
 
     @Override
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRED)
     public void deleteUser(long userId) {
         userDao.deleteUser(userId);
     }
 
     @Override
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRED)
     public void addUser(String name, String lastName, String email) {
         userDao.add(new User(name, lastName, email));
     }
 
     @Override
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRED)
     public void updateUser(long id, String name, String lastName, String email) {
         userDao.updateUser(id, name, lastName, email);
     }
